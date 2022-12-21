4aa4d39c043e58a17c1dd1bfc174aec4a8fc61f6
orianemgn <mgnoriane@gmail.com>
2022-12-21 11:57:57 +0100

    music-app sql done - connect the backend is not working

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 
+const { server_port } = process.env;
 const app = express();
+
+app.listen(server_port, (e) => {
+  if (e) {
+    console.log(e);
+  } else {
+    console.log('listen to port', server_port);
+  }
+});
 
 // Please keep this module.exports app, we need it for the tests !
 module.exports = app;
