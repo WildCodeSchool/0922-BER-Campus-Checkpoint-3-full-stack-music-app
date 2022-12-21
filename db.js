f332119b93dc18e3c5c9bf8ffbcdcc529f6a2575
Vladyslava Klovak <vladaklovak@gmail.com>
2022-12-21 11:11:58 +0100

    connect to db, add migration handler

---
+++ db.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  multipleStatements: true,
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("db connected");
+});
+module.exports = connection;
