ad01bc3fcab2da7098be7cad9d14b8c0fbff87ff
Lars Poetzel <lars.poetzel@gmail.com>
2022-12-21 11:24:22 +0100

    database running, getTracks,getTracksbyID

--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
 require('dotenv').config();
 const express = require('express');
+const connection = require('../db');
 
 const app = express();
 
+connection
+  .promise()
+  .query('use CP3;')
+  .catch((e) => console.error(e));
+
+app.get('/', (req, res) => {
+  res.send('welcome to main route');
+});
+
+app.get('/api/tracks', (req, res) => {
+  const sqlQuery = 'select * from track';
+  connection
+    .promise()
+    .query(sqlQuery)
+    .then(([result]) => res.send(result));
+});
+
+app.get('/api/tracks/:id', (req, res) => {
+  let { id } = req.params;
+  connection
+    .promise()
+    .query('SELECT * FROM track WHERE id_track = ?', [id])
+    .then(([results]) => {
+      if (!results.length) {
+        res.status(404).send({
+          status: '404',
+          msg: 'Not found',
+          data: null,
+        });
+      } else {
+        res.json(results);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving track from db');
+    });
+});
 // Please keep this module.exports app, we need it for the tests !
 module.exports = app;
